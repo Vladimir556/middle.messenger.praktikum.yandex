687ea6e110d5ed7e599092e48d17a2a9150f41d7
Vladimir <smelov.vladimir133@gmail.com>
2023-01-20 01:45:18 +0500

    hot fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
